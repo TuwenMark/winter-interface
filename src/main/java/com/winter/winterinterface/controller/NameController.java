46afaf4e3eacb9343fe8b07c21789c1fbb3486e7
MarkTuwen <m18698577021@163.com>
2023-02-08 21:06:51 +0800

    add get through the gateway function

--- src/main/java/com/winter/winterinterface/controller/NameController.java
+++ src/main/java/com/winter/winterinterface/controller/NameController.java
@@ -1,12 +1,9 @@
 package com.winter.winterinterface.controller;
 
 import com.winter.winterapiclientsdk.model.User;
-import com.winter.winterinterface.util.SignUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @program: winter-interface
@@ -30,32 +27,32 @@ public class NameController {
 
 	@PostMapping("/user")
 	public String getNameByPost(@RequestBody User user, HttpServletRequest request) {
-		String accessKey = request.getHeader("accessKey");
-		// TODO: 2022/11/12 通过accessKey在数据库查询到secretKey
-		String serverSecretKey = "abcdefg";
-		String requestParams = request.getHeader("requestParams");
-		String nonce = request.getHeader("nonce");
-		String timestamp = request.getHeader("timestamp");
-		String clientSign = request.getHeader("sign");
-//		if (!"winter".equals(accessKey) && !"abcdefg".equals(secretKey)) {
+//		String accessKey = request.getHeader("accessKey");
+//		// TODO: 2022/11/12 通过accessKey在数据库查询到secretKey
+//		String serverSecretKey = "abcdefg";
+//		String requestParams = request.getHeader("requestParams");
+//		String nonce = request.getHeader("nonce");
+//		String timestamp = request.getHeader("timestamp");
+//		String clientSign = request.getHeader("sign");
+////		if (!"winter".equals(accessKey) && !"abcdefg".equals(secretKey)) {
+////			throw new RuntimeException("无权限");
+////		}
+//		// TODO: 2022/11/12  校验nonce
+//
+//		//  校验timestamp，在5分钟之内有效
+//		if (System.currentTimeMillis() > Long.valueOf(timestamp) + 5*60*1000) {
+//			throw  new RuntimeException("无权限");
+//		}
+//		// 校验sign
+//		Map<String, String> headerMap= new HashMap<>();
+//		headerMap.put("accessKey", accessKey);
+//		headerMap.put("requestParams", requestParams);
+//		headerMap.put("nonce", nonce);
+//		headerMap.put("timestamp", timestamp);
+//		String serverSign = SignUtils.genSign(headerMap, serverSecretKey);
+//		if (!serverSign.equals(clientSign)) {
 //			throw new RuntimeException("无权限");
 //		}
-		// TODO: 2022/11/12  校验nonce
-
-		//  校验timestamp，在5分钟之内有效
-		if (System.currentTimeMillis() > Long.valueOf(timestamp) + 5*60*1000) {
-			throw  new RuntimeException("无权限");
-		}
-		// 校验sign
-		Map<String, String> headerMap= new HashMap<>();
-		headerMap.put("accessKey", accessKey);
-		headerMap.put("requestParams", requestParams);
-		headerMap.put("nonce", nonce);
-		headerMap.put("timestamp", timestamp);
-		String serverSign = SignUtils.genSign(headerMap, serverSecretKey);
-		if (!serverSign.equals(clientSign)) {
-			throw new RuntimeException("无权限");
-		}
 		return "Post 你的用户名是：" + user.getUsername();
 	}
 }
